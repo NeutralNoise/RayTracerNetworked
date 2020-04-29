24b8dd65152e752c3f755e568d82c0c58491777b
NeutralNoise <classicbrad24@gmail.com>
2020-04-30 02:24:30 +1000

    Uses new SDL2 setup.

--- RayTracerNetworked/Server/main.cpp
+++ RayTracerNetworked/Server/main.cpp
@@ -4,23 +4,62 @@
 #include <iostream>
 #include <string>
 #include "SDL2/SDL.h"
+#include "Renderer.h"
 
 int main(int argc, char ** argv)
 {
     std::cout << "Hello World!\n";
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-		std::cout << "SDL2 Error\n";
-	}
-	else {
-		std::cout << "SDL2 OK\n";
-	}
+	int x = 0;
+	int y = 0;
+	Renderer render;
+	render.InitRenderer(800, 800);
+	bool running = true;
+	while (running) {
+		SDL_Event ievent;
+		while (SDL_PollEvent(&ievent) != 0) {
+			switch (ievent.type) {
+			case::SDL_QUIT:
+				running = false;
+			}
+		}
+		if (y >= 799) {
+			std::cout << "Completed Image!\n";
+			std::cin.get();
+			exit(0);
+		}
+		else {
+			render.LockTexture();
+			for (int i = x; i <= x + 50; i++) {
+				for (int t = y; t <= y + 50; t++) {
+					render.SetPixel(i, t, 128, 0, 128);
+				}
+			}
+			render.UnlockTexture();
+		}
 
-	SDL_version sdlVersion;
-	SDL_GetVersion(&sdlVersion);
-	std::cout << std::to_string(sdlVersion.major) << "." << std::to_string(sdlVersion.minor) << "." << std::to_string(sdlVersion.patch) << "\n";
+		if (x < 799) {
+			x += 50;
+			//Just to make sure we don't go where we shouldn't
+			if (x >= 799) {
+				x = 0;
+				if (y < 799) {
+					y += 50;
+				}
+			}
+		}
+		else {
+			x = 0;
+			if (y < 799) {
+				y += 50;
+			}
+		}
+		render.Draw();
 
-	std::cin.get();
+	}
+	render.Clean();
+
+	//std::cin.get();
 	return 0;
 }
 
