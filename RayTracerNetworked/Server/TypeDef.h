cd6e6f3cb0446082319c80f3aa2a533e94fd274d
NeutralNoise <classicbrad24@gmail.com>
2020-04-30 02:22:51 +1000

    Added typedefs for uint/int 8/32/64

---
+++ RayTracerNetworked/Server/TypeDef.h
@@ -0,0 +1,13 @@
+#ifndef TYPEDEF_H_INCLUDED
+#define TYPEDEF_H_INCLUDED
+
+
+typedef unsigned int UInt32;
+typedef int Int32;
+typedef unsigned long long UInt64;
+typedef long long Int64;
+typedef unsigned char UInt8;
+typedef char Int8;
+
+#endif // !TYPEDEF_H_INCLUDED
+
