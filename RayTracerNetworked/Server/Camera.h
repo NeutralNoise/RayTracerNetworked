bf2a507a80a8b73420a65e24ab3058114a2fbb78
NeutralNoise <classicbrad24@gmail.com>
2020-05-04 03:56:27 +1000

    Added Camera class.

---
+++ RayTracerNetworked/Server/Camera.h
@@ -0,0 +1,44 @@
+#ifndef CAMERA_H_INCLUDED
+#define CAMERA_H_INCLUDED
+#include "Vector.h"
+
+struct CameraRotation
+{
+	Vector CameraX;
+	Vector CameraY;
+	Vector CameraZ;
+};
+
+class Camera
+{
+	Vector GetPosition() {
+		return m_position;
+	}
+
+	void SetPosition(const Vector &v) {
+		m_position = v;
+	}
+
+	void SetRotation(const CameraRotation &r) {
+		//Just use this for now.
+		m_rotation.CameraZ = m_position.Normalize();
+		m_rotation.CameraX = m_rotation.CameraZ.CrossProduct(Vector(0.0f, 0.0f, 1.0f)).Normalize();
+		m_rotation.CameraY = m_rotation.CameraZ.CrossProduct(m_rotation.CameraX).Normalize();
+	}
+
+	float GetFov() const {
+		return m_fov;
+	}
+
+	void SetFov(const float &fov) {
+		m_fov = fov;
+	}
+
+private:
+	Vector m_position;
+	CameraRotation m_rotation;
+	float m_fov;
+
+};
+
+#endif // !CAMERA_H_INCLUDED
