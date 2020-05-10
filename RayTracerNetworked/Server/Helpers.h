a5357ad36ac06ebdb4fb740ff4998638b212e40e
NeutralNoise <classicbrad24@gmail.com>
2020-05-10 12:05:30 +1000

    Added chunk calculation functions.

--- RayTracerNetworked/Server/Helpers.h
+++ RayTracerNetworked/Server/Helpers.h
@@ -5,6 +5,7 @@
 #include "Material.h"
 #include <math.h>
 #include "Ray.h"
+#include "Config.h"
 #include <random>
 #include <limits>
 #include <vector>
@@ -79,6 +80,65 @@ public:
 			return (float)((float)(rnd) / 101.0f);
 		}
 	};
+
+	static void MaxChunks(int * w, int *h) {
+		*w = Config::ImageWidth / Config::ChunkWidth; //This might be wrong.
+		*h = Config::ImageHeight / Config::ChunkWidth;
+	}
+
+	static void MaxChunks(const int &cw, const int &ch,int * w, int *h) {
+		*w = Config::ImageWidth / cw; //This might be wrong.
+		*h = Config::ImageHeight / ch;
+	}
+
+	static void GetChunkXY(const int &chunkID, int * x, int *y) {
+
+		int chunkW;
+		int chunkH;
+
+		MaxChunks(&chunkW, &chunkH);
+
+		int chunkX = chunkID % chunkW;
+		int chunkY = (int)floor(chunkID / chunkH);
+		*x = Config::ChunkWidth * chunkX;
+		*y = Config::ChunkHeight * chunkY;
+	}
+
+	static void GetChunkArea(const int &chunkID, int * x, int *y, int * w, int * h) {
+
+		int chunkW;
+		int chunkH;
+
+		MaxChunks(&chunkW, &chunkH);
+
+		int chunkX = chunkID % chunkW;
+		int chunkY = (int)floor(chunkID / chunkH);
+		*x = Config::ChunkWidth * chunkX;
+		*y = Config::ChunkHeight * chunkY;
+		*w = *x + Config::ChunkWidth;
+		*h = *y + Config::ChunkHeight;
+	}
+
+	static void GetChunkArea(const int &chunkID, const int &cw, const int &ch, int * x, int *y, int * w, int * h) {
+
+		int chunkW;
+		int chunkH;
+
+		MaxChunks(cw, ch, &chunkW, &chunkH);
+
+		int chunkX = chunkID % chunkW;
+		int chunkY = (int)floor(chunkID / chunkH);
+		*x = cw * chunkX;
+		*y = ch * chunkY;
+		*w = *x + cw;
+		*h = *y + ch;
+	}
+
+	static int MaxChunks(const int &w, const int &h, const int &cw, const int &ch) {
+		int maxChunksX = w / cw; //This might be wrong.
+		int maxChunksY = h / ch;
+		return maxChunksX * maxChunksY;
+	}
 
 };
 
